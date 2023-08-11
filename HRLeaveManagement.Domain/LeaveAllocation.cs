8d576d97cc7182b23806916000b68482fa072a3f
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-08-11 21:53:50 +0400

    Add project files.

---
+++ HRLeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,16 @@
+﻿using HR.LeaveManagement.Domain.Common;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HR.LeaveManagement.Domain
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfDays { get; set; }
+
+        public LeaveType? LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public int PeriodId { get; set; }
+    }
+}
