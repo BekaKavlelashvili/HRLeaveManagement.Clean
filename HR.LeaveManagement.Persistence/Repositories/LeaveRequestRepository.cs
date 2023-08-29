a8b4ef3095f8dc07fecef92d61fe1dc5d74266d1
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-08-29 16:57:07 +0400

    add completed repository implementations

--- HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
+++ HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -1,6 +1,7 @@
 ﻿using HR.LeaveManagement.Application.Contracts.Persistence;
 using HR.LeaveManagement.Domain;
 using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace HR.LeaveManagement.Persistence.Repositories
 {
@@ -9,6 +10,21 @@ namespace HR.LeaveManagement.Persistence.Repositories
         public LeaveRequestRepository(HrDatabaseContext context) : base(context)
         {
 
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
+        {
+            return await _context.LeaveRequests.Include(x => x.LeaveType).ToListAsync();
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId)
+        {
+            return await _context.LeaveRequests.Where(x => x.RequestingEmployeeId == userId).Include(x => x.LeaveType).ToListAsync();
+        }
+
+        public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+        {
+            return await _context.LeaveRequests.Include(x => x.LeaveType).FirstOrDefaultAsync(x => x.Id == id);
         }
     }
 }
