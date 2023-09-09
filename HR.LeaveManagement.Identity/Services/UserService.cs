3e30b9d250d926963daa5667dece7ae61ee77fdc
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-09-09 14:35:20 +0400

    add security using identity

---
+++ HR.LeaveManagement.Identity/Services/UserService.cs
@@ -0,0 +1,48 @@
+﻿using HR.LeaveManagement.Application.Contracts.Identity;
+using HR.LeaveManagement.Application.Models.Identity;
+using HR.LeaveManagement.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Identity.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(UserManager<ApplicationUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+        public async Task<Employee> GetEmployee(string userId)
+        {
+            var employee = await _userManager.FindByIdAsync(userId);
+
+            return new Employee
+            {
+                Id = employee.Id,
+                Email = employee.Email,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+            };
+        }
+
+        public async Task<List<Employee>> GetEmployees()
+        {
+            var employees = await _userManager.GetUsersInRoleAsync("Employee");
+
+            return employees.Select(x => new Employee
+            {
+                Id = x.Id,
+                Email = x.Email,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+            }).ToList();
+        }
+    }
+}
